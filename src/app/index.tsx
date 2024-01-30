1c8089fe39ad69c81ce8e248eb31d0de3f2f2fdd
vinaynarayankutty <vinaynarayankutty@gmail.com>
2024-01-30 14:14:20 +0530

    Integrated expo router and configured ts paths

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function WeatherScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
